8dd77a7ffbb58fdac7512ade9531700e3c9a2952
Hans-Peter Bock <hpbock@avaapgh.de>
2018-12-11 21:48:32 +0100

    added lnd grpc and request to forwardinghistory

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-function hello() {
-    console.log(`Hello world!\nUsing ${process.version} node version.`);
+const fs = require('fs');
+const grpc = require('grpc');
+const lnrpc = grpc.load('rpc.proto').lnrpc;
+
+const program = require('commander');
+program
+    .version('1.0.0', '-v, --version')
+    .description('plot a sankey diagram of the forwarding history')
+    .option('--lnd.macaroon [base64|path]', 'Base64 encoded string or path to macaroon', process.env.LND_MACAROON || '/root/.lnd/invoice.macaroon')
+    .option('--lnd.rpccert [base64|path]', 'Base64 encoded string or path to TLS certificate for lnd\'s RPC services', process.env.LND_RPC_CERT || '/root/.lnd/tls.cert')
+    .option('--lnd.rpcserver [server]', 'Interface/port to lnd\'s RPC services', process.env.LND_RPC_SERVER || 'localhost:10009')
+    .option('--listen [server]', 'Interface/port to the web app', process.env.LISTEN || 'localhost:3000')
+    .parse(process.argv)
+
+let lndMacaroon
+try {
+    // try to get macaroon from path
+    lndMacaroon = fs.readFileSync(program['lnd.macaroon']).toString('hex');
+} catch (err) {
+    // it's probably a base64 encoded string then
+    lndMacaroon = Buffer.from(program['lnd.macaroon'], 'base64').toString('hex');
 }
 
-hello();
+let lndCert
+try {
+    // try to get certificate from path
+    lndCert = fs.readFileSync(program['lnd.rpccert'])
+} catch (err) {
+    // it's probably a base64 encoded string then
+    lndCert = Buffer.from(program['lnd.rpccert'], 'base64')
+}
+
+process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA'
+const sslCreds = grpc.credentials.createSsl(lndCert);
+const macaroonCreds = grpc.credentials.createFromMetadataGenerator(function(args, callback) {
+    var metadata = new grpc.Metadata()
+    metadata.add('macaroon', lndMacaroon);
+    callback(null, metadata);
+});
+const creds = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds);
+const lightning = new lnrpc.Lightning(program['lnd.rpcserver'], creds);
+
+var request = {}
+lightning.forwardingHistory(request, function(err, response) {
+    console.log(response);
+});
